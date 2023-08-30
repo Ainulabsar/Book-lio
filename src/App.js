2d1edfcdc5211e411d2f6ecd3472bba9e503e740
Mohammed Ainul Absar <absarainul027@gmail.com>
2023-08-30 22:25:53 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -7,49 +7,18 @@ import BookDetail from "./components/BookDetail";
 import { Routes, Route } from "react-router-dom";
 import Book from "./components/Buyer/Book";
 import Bill from "./components/Buyer/Bill";
-import Register from "./components/Register/Register";
-import Login from "./components/Login/Login";
-
-// function App() {
-//   return (
-//     <>
-//       <Nav />
-//       <Register />
-
-//       <Routes>
-//         {/* <Route path="/Signup" element={<Register />}></Route> */}
-//         <Route path="/" element={<Home />}></Route>
-//         <Route path="/About" element={<About />}></Route>
-//         <Route path="/Add" element={<Add />}></Route>
-//         <Route path="/books/:id" element={<BookDetail />}></Route>
-//         <Route path="/Book" element={<Book />}></Route>
-//         <Route path="/Bill" element={<Bill />}></Route>
-//       </Routes>
-//     </>
-//   );
-// }
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/register" element={<Register />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/" element={<MainWithNav />} />
-    </Routes>
-  );
-}
-
-function MainWithNav() {
   return (
     <>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/books/:id" element={<BookDetail />} />
-        <Route path="/book" element={<Book />} />
-        <Route path="/bill" element={<Bill />} />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Add" element={<Add />}></Route>
+        <Route path="/books/:id" element={<BookDetail />}></Route>
+        <Route path="/Book" element={<Book />}></Route>
+        <Route path="/Bill" element={<Bill />}></Route>
       </Routes>
     </>
   );
