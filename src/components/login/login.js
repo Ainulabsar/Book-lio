0b9047d46b19a24cb9819821cdbb9706e970a79f
Mohammed Ainul Absar <absarainul027@gmail.com>
2023-10-14 22:53:48 +0530

    Added style to login and sign up page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -40,132 +40,106 @@ function Login() {
     }
   }
   return (
-    <div className="login">
-      <h3>Login</h3>
-      <form action="post">
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            placeholder="Enter Email Address"
-            className="form-control"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
+    <div className="containerp">
+      <div className="containerc">
+        <h3>Login</h3>
+        <form action="post">
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email Address"
+              className="form-control"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
           </div>
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            onChange={(e) => {
-              setPasswword(e.target.value);
-            }}
-            placeholder="Enter Password"
-            id="exampleInputPassword1"
-          />
-        </div>
-        {/* <div className="dropdown">
-          <button
-            id="btn"
-            className="btn btn-primary dropdown-toggle"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Please Select Your Domain
-          </button>
-          <ul className="dropdown-menu">
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              onChange={(e) => {
+                setPasswword(e.target.value);
+              }}
+              placeholder="Enter Password"
+              id="exampleInputPassword1"
+            />
+          </div>
 
-            <li>
-              <a className="dropdown-item">
-                Seller
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" >
-                Buyer
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item">
-                Admin
-              </a>
-            </li>
-          </ul>
-        </div> */}
-        <label>Please Select Your Domain</label>
-        <br />
-        <div className="form-check form-check-inline" id="radiobtn">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio1"
-            value="option1"
-            onChange={(e) => {
-              setSeller(e.target.value);
-            }}
-          />
-          <label className="form-check-label" for="inlineRadio1">
-            Seller
-          </label>
-        </div>
-        <div className="form-check form-check-inline">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio2"
-            value="option2"
-            onChange={(e) => {
-              setBuyer(e.target.value);
-            }}
-          />
-          <label className="form-check-label" for="inlineRadio2">
-            Buyer
-          </label>
-        </div>
-        <div className="form-check form-check-inline">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio3"
-            value="option3"
-            disabled
-          />
-          <label className="form-check-label" for="inlineRadio3">
-            Admin (disabled)
-          </label>
-        </div>
-        <div className="d-grid gap-2">
-          <button type="submit" className="btn btn-primary" onClick={submit}>
-            Submit
-          </button>
-        </div>
-        <br />
-        <div id="emailHelp" className="form-text">
-          if you are not a User Please Register !
-        </div>
-        <div className="d-grid gap-2">
-          <button type="button" className="btn btn-primary">
-            <Link to={"/signup"} className="log">
-              Signup
-            </Link>
-          </button>
-        </div>
-      </form>
+          <label>Please Select Your Domain</label>
+          <br />
+          <div className="form-check form-check-inline" id="radiobtn">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio1"
+              value="option1"
+              onChange={(e) => {
+                setSeller(e.target.value);
+              }}
+            />
+            <label className="form-check-label" for="inlineRadio1">
+              Seller
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio2"
+              value="option2"
+              onChange={(e) => {
+                setBuyer(e.target.value);
+              }}
+            />
+            <label className="form-check-label" for="inlineRadio2">
+              Buyer
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio3"
+              value="option3"
+              disabled
+            />
+            <label className="form-check-label" for="inlineRadio3">
+              Admin (disabled)
+            </label>
+          </div>
+          <div className="d-grid gap-2">
+            <button type="submit" className="btn btn-primary" onClick={submit}>
+              Submit
+            </button>
+          </div>
+          <br />
+          <div id="emailHelp" className="form-text">
+            if you are not a User Please Register !
+          </div>
+          <div className="d-grid gap-2">
+            <button type="button" className="btn btn-primary">
+              <Link to={"/signup"} className="lo">
+                Signup
+              </Link>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -58,132 +58,138 @@ function Signup() {
   }
 
   return (
-    <div className="login">
-      <div>
-        <h3 className="sh">Sign-up</h3>
-        <form action="post">
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
-              User Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              placeholder="Enter User Name"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              placeholder="Enter Email Address"
-              className="form-control"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
+    <div className="log">
+      <div className="login">
+        <div>
+          <h3 className="sh">Sign-up</h3>
+          <form action="post">
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">
+                User Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                placeholder="Enter User Name"
+                id="exampleInputPassword1"
+              />
             </div>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              onChange={(e) => {
-                setPasswword(e.target.value);
-              }}
-              placeholder="Enter Password"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
-              Confim Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              onChange={(e) => {
-                setConpasswword(e.target.value);
-              }}
-              placeholder="Enter Password"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <label>Please Select Your Domain</label>
-          <br />
-          <div className="form-check form-check-inline" id="radiobtn">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio1"
-              value="option1"
-              onChange={(e) => {
-                setSeller(e.target.value);
-              }}
-            />
-            <label className="form-check-label" for="inlineRadio1">
-              Seller
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio2"
-              value="option2"
-              onChange={(e) => {
-                setBuyer(e.target.value);
-              }}
-            />
-            <label className="form-check-label" for="inlineRadio2">
-              Buyer
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="inlineRadioOptions"
-              id="inlineRadio3"
-              value="option3"
-              disabled
-            />
-            <label className="form-check-label" for="inlineRadio3">
-              Admin (disabled)
-            </label>
-          </div>
-          <div className="d-grid gap-2">
-            <button type="submit" className="btn btn-primary" onClick={submit}>
-              Submit
-            </button>
-          </div>
-          <br />
-          <div id="emailHelp" className="form-text">
-            if you Already Have an Account Please <strong>Login</strong> !
-          </div>
-          <div className="d-grid gap-2">
-            <button type="submit" className="btn btn-primary">
-              <Link to={"/"} className="log">
-                Login
-              </Link>
-            </button>
-          </div>
-        </form>
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Email Address"
+                className="form-control"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                onChange={(e) => {
+                  setPasswword(e.target.value);
+                }}
+                placeholder="Enter Password"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">
+                Confim Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                onChange={(e) => {
+                  setConpasswword(e.target.value);
+                }}
+                placeholder="Enter Password"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <label>Please Select Your Domain</label>
+            <br />
+            <div className="form-check form-check-inline" id="radiobtn">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio1"
+                value="option1"
+                onChange={(e) => {
+                  setSeller(e.target.value);
+                }}
+              />
+              <label className="form-check-label" for="inlineRadio1">
+                Seller
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio2"
+                value="option2"
+                onChange={(e) => {
+                  setBuyer(e.target.value);
+                }}
+              />
+              <label className="form-check-label" for="inlineRadio2">
+                Buyer
+              </label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio3"
+                value="option3"
+                disabled
+              />
+              <label className="form-check-label" for="inlineRadio3">
+                Admin (disabled)
+              </label>
+            </div>
+            <div className="d-grid gap-2">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={submit}
+              >
+                Submit
+              </button>
+            </div>
+            <br />
+            <div id="emailHelp" className="form-text">
+              if you Already Have an Account Please <strong>Login</strong> !
+            </div>
+            <div className="d-grid gap-2">
+              <button type="submit" className="btn btn-primary">
+                <Link to={"/"} className="lo">
+                  Login
+                </Link>
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
